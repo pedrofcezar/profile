7462c15abd2221a5f4e04ce06f28e260024ad989
Pedro Cezar <pedrofcezar@gmail.com>
2018-07-12 11:26:22 -0300

    my profile page

---
+++ images/(2) Pedro Cezar_files/VAR5FN37ILq.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["MnMMm"]); }
+
+__d("BanzaiConsts",[],(function(a,b,c,d,e,f){a={SEND:"Banzai:SEND",OK:"Banzai:OK",ERROR:"Banzai:ERROR",SHUTDOWN:"Banzai:SHUTDOWN",VITAL_WAIT:1e3,BASIC_WAIT:6e4,EXPIRY:30*6e4};e.exports=a}),null);
---
+++ images/(2) Pedro Cezar_files/XOIv9M6Nwai.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["njZWU"]); }
+
+__d("BanzaiLogger",["Banzai"],(function(a,b,c,d,e,f){var g="logger";function a(a){return{log:function(c,d){b("Banzai").post(h._getRoute(c),d,a)},registerToSendWithBeacon:function(a,c,d,e){b("Banzai").registerToSendWithBeacon(h._getRoute(a),c,d,e)},_getRoute:function(a){return g+":"+a}}}var h=a();h.create=a;e.exports=h}),null);
